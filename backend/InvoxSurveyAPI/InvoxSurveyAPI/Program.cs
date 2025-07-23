976b4e32979bda0a6e8763a73a84b9d8745a70f8
Jesus Romero <elherzt@gmail.com>
2025-07-23 12:37:27 -0600

    configure dockers and postgresql connection

--- backend/InvoxSurveyAPI/InvoxSurveyAPI/Program.cs
+++ backend/InvoxSurveyAPI/InvoxSurveyAPI/Program.cs
@@ -1,7 +1,15 @@
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+//DB Configuration
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres")));
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
